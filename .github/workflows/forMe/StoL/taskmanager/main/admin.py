64735bba7b57180d5b952827ef4284e39a8d6dcd
GeorgProfi <98205181+GeorgProfi@users.noreply.github.com>
2022-08-07 07:55:01 +0300

    Delete .github/workflows/forMe directory

--- .github/workflows/forMe/StoL/taskmanager/main/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import list_of_executed
-
-admin.site.register(list_of_executed)
--- .github/workflows/forMe/StoL/taskmanager/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.index, name = 'Global' ),
-    path ('about',views.about, name = 'about'),
-    path ('list_E',views.list_E, name = 'list_E')
-]
